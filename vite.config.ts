6b5c772edf3e9b57e2099d29ee83c1616815c8f7
golla-sreenivas-yadav <Srinivaspa374@gmail.com>
2025-10-01 23:18:42 +0530

    Vite config update for blank page fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+     base: '/', 
     plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
